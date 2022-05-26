898961b03287ab58ee4bd530360d5421b6b35096
Amir <amirmohamadrayeji@gmail.com>
2022-05-26 13:18:59 +0430

    create post detail page

--- theory/urls.py
+++ theory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('',views.PostListView.as_view(), name='post_list'),
+    path('detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- theory/views.py
+++ theory/views.py
@@ -4,8 +4,14 @@ from theory.models import Post
 
 
 class PostListView(generic.ListView):
-    template_name = 'blog/post_list.html'
+    template_name = 'theory/post_list.html'
     context_object_name = 'post_list'
 
     def get_queryset(self):
         return Post.objects.filter(status='pub').order_by('-datetime_modified')
+
+
+class PostDetailView(generic.DetailView):
+    template_name = 'theory/post_detail.html'
+    model = Post
+    context_object_name = 'post'
