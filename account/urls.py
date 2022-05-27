feb8fe3722693dea257c8e25e4d68894344730ac
Amir <amirmohamadrayeji@gmail.com>
2022-05-27 13:55:41 +0430

    add signup and logout pages

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from account import views
+
+
+urlpatterns = [
+   path('signup/', views.SignUpView.as_view(), name='signup'),
+]
--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theory.urls')),
     path('account/', include('django.contrib.auth.urls')),
+    path('account/', include('account.urls')),
 ]
