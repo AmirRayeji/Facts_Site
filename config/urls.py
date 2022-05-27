3f5c150de64b790b8de1e3cb0e882508f6beb942
Amir <amirmohamadrayeji@gmail.com>
2022-05-27 13:19:41 +0430

    create newapp account and setting up login page

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theory.urls')),
+    path('account/', include('django.contrib.auth.urls')),
 ]
